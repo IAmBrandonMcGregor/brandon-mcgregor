744d46c7657281ff2ed468de754f6b9605b609e1
IAmBrandonMcGregor <iambrandonmcgregor@gmail.com>
2013-03-04 22:42:38 -0500

    Did some work. Not much though.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('brandonMcgregorApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Testacular'
-    ];
-  }]);
+.controller('MainCtrl', ['$scope', function ($scope) {
+
+	// A collection of professional (aka paid) projects.
+	$scope.professional = [
+		{ name: 'Salespad.net' },
+		{ name: '1023software.com' },
+		{ name: 'Salespad Cloud' }
+	];
+
+	// A collection of amateur projects.
+	$scope.amateur = [
+		{ name: 'Web Touch Controller' }
+	];
+
+
+	$scope.ShowProjectDetails = function (project) {
+		console.log("This should open the project '" + project.name + "'.");
+	};
+}]);
