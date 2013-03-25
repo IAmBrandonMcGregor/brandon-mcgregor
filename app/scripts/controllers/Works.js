55cb50c8b0fa328eb8905bf63f1283dfb53f42d7
IAmBrandonMcGregor <iambrandonmcgregor@gmail.com>
2013-03-25 18:31:08 -0400

    Laying out the 'works' page still.

--- app/scripts/controllers/Works.js
+++ app/scripts/controllers/Works.js
@@ -3,4 +3,31 @@
 angular.module('brandonMcgregorApp')
 .controller('WorksCtrl', ['$scope', function ($scope) {
 	$scope.name = "Works";
+
+	$scope.works = [
+		{
+			name : "This Website",
+			code_location : "www.github.com/IAmBrandonMcGregor/brandon-mcgregor",
+			live_location: "You're there."
+		},
+		{
+			name : "Salespad Website",
+			code_location : "Proprietary",
+			live_location: "http://www.salespad.net"
+		},
+		{
+			name : "1023 Software Website",
+			code_location : "Proprietary",
+			live_location : "http://www.1023software.com"
+		},
+		{
+			name : "Brave Plastic",
+			code_location : "www.github.com/IAmBrandonMcGregor/brave-plastic",
+			live_location : "www.brandonmcgregor.com/BravePlastic"
+		}
+	];
+
+	
+
+	$scope.project_style = { width: ((100 / $scope.works.length) + "%") };
 }]);
