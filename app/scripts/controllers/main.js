14da613d5ad14a85fbca6389a17f711e81dc67d9
IAmBrandonMcGregor <iambrandonmcgregor@gmail.com>
2013-03-25 10:52:09 -0400

    Added screen size capture binding.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,13 +2,17 @@
 
 angular.module('brandonMcgregorApp')
 .controller('MainCtrl', ['$scope', 'WindowSrvc', function ($scope, WindowSrvc) {
+	$scope.container_styling = { width: '200px' };
 
 	// Once the page has loaded, call the Window Service to read the screen's dimensions.
 	(function ($, WindowSrvc) {
+
 		// Read the screen dimensions once the screen has rendered.
 		$(window).ready(function () {
 			WindowSrvc.CaptureScreenDimensions();
+			$scope.container_styling.width = WindowSrvc.main_page.width + 'px';
 		}.bind(WindowSrvc));
+
 	})(jQuery, WindowSrvc);
 	
 }]);
